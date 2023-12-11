7c03d7705724482fe12eeb839664360772563c95
Luis Hlatki <luis.hlatki@hotmail.com>
2023-12-11 20:10:30 -0300

    added a way to auth using certificates

--- routes/payins-certificate-auth-get-payments.js
+++ routes/payins-certificate-auth-get-payments.js
@@ -1,54 +1,37 @@
-// Import necessary modules
 const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const https = require('https');
 const crypto = require('crypto');
 
-// Create an Express router
 const router = express.Router();
 
 // Function to generate HMAC SHA256 signature
 function generateHmacSha256Signature(payload, secretKey) {
-  // Create an HMAC SHA256 hash using the provided payload and secret key
   const hmac = crypto.createHmac('sha256', secretKey);
   hmac.update(payload);
-  // Return the hexadecimal representation of the hash
   return hmac.digest('hex');
 }
 
-// SSL certificate options
 const sslOptions = {
-  key: fs.readFileSync('./misc/YOUR-private-key.pem'),
-  cert: fs.readFileSync('./misc/YOUR-server.crt.pem'),
+  key: fs.readFileSync('./misc/private-key.pem'),
+  cert: fs.readFileSync('./misc/server.crt.pem'),
 };
 
-// Define a route for handling POST requests
 router.post('/', async (req, res) => {
-  // API endpoint URL
-  const apiURL = 'https://sandbox-cert.dlocal.com/payments-methods?country';
-  
-  // Generate a timestamp for the request
+  const apiURL = 'https://api-certs.dlocal.com/payments-methods?country';
   const timestamp = new Date().toISOString();
-  
-  // Authentication credentials
-  const login = 'X';
-  const transKey = 'X';
-  const secretKey = 'X';
-  
+  const login = 'x';
+  const transKey = 'x';
+  const secretKey = 'x';
   // Replace this with your actual request payload
   const requestPayload = req.body;
 
-  // Concatenate data for creating the HMAC signature
   const concatenatedData = `${login}${timestamp}`;
-  
-  // Convert secret key to bytes
   const keyBytes = Buffer.from(secretKey, 'utf-8');
 
-  // Generate HMAC SHA256 signature
   const hashBytes = generateHmacSha256Signature(concatenatedData, keyBytes);
 
-  // Set headers for the HTTP request
   const headers = {
     'X-Date': timestamp,
     'X-Login': login,
@@ -56,29 +39,21 @@ router.post('/', async (req, res) => {
     Authorization: `V2-HMAC-SHA256, Signature: ${hashBytes}`,
   };
 
-  // Create an HTTPS agent with SSL options
   const agent = new https.Agent({
     key: sslOptions.key,
     cert: sslOptions.cert
   });
 
   try {
-    // Make a GET request to the API with authentication headers and payload
     const response = await axios.get(`${apiURL}=${requestPayload.country}`, { headers, httpsAgent: agent });
-    
-    // Log the response details
     console.log(`Response: ${JSON.stringify(response.data)}`);
     console.log(`Header: ${JSON.stringify(response.headers)}`);
-    
-    // Send a JSON response indicating success
     res.json({ message: 'Payment request successful', response: response.data });
   } catch (error) {
-    // Log and handle errors in case the payment request fails
     console.error(`Payment request failed. Headers: ${error.response.headers}`);
     console.error(`Payment request failed. Message: ${error}`);
     res.status(500).json({ error: `Payment request failed. Error: ${error.response.data.message}` });
   }
 });
 
-// Export the router for use in other modules
 module.exports = router;
