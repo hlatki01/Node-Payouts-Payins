f890666c0c0de97bdd830091d974574a7cfa2eb4
Luis Hlatki <luis.hlatki@hotmail.com>
2023-12-11 17:05:31 -0300

    added a way to auth using certificates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const payoutsRoute = require('./routes/payouts');
 const payinsRoute = require('./routes/payins');
+const payinsCertificateAuthRoute = require('./routes/payins-certificate-auth');
+const payinsCertificateAuthRouteGetPaymentMethods = require('./routes/payins-certificate-auth-get-payments');
 const notificationsRoute = require('./routes/notifications');
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(express.json());
 // Use the routes
 app.use('/payouts', payoutsRoute);
 app.use('/payins', payinsRoute);
+app.use('/payins-certificate-auth-get-payments', payinsCertificateAuthRouteGetPaymentMethods);
+app.use('/payins-certificate-auth', payinsCertificateAuthRoute);
 app.use('/notifications', notificationsRoute);
 
 // Start the server
